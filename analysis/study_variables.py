c8a7158fbe1e41cf3e95678045064529123d5a6d
Matt1978b <matthew.carr@manchester.ac.uk>
2021-10-20 14:29:34 +0100

    Adding diabetes dates (GP)

--- analysis/study_variables.py
+++ analysis/study_variables.py
@@ -44,9 +44,85 @@ def generate_study_variables(index_date_variable):
                 },
             },
         ),
-        #### diabetes_diagnosis=
+        # Diabetes
+        # Date of first (any diabetes diagnosis) in primary care
+        diabetes_diagnosis=patients.with_these_clinical_events(
+            combine_codelists(
+                diabetes_t1_codes, diabetes_t2_codes, diabetes_unknown_codes
+            ),
+            on_or_before="today",
+            return_first_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={"incidence": 0.05},
+        ),
+
+        # For stratifying on type of diabetes (at discharge)
+        # And for identifying 'new onset' diabetes 
+        t1dm_gp_first=patients.with_these_clinical_events(
+            diabetes_t1_codes,
+            on_or_before=f"{index_date_variable} + 30 days",
+            return_first_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={
+                "date": {"earliest": "2015-01-01"},
+                "incidence": 0.01
+            },
+        ),
+        t1dm_gp_last=patients.with_these_clinical_events(
+            diabetes_t1_codes,
+            on_or_before=f"{index_date_variable} + 30 days",
+            return_last_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={
+                "date": {"earliest": "2015-01-01"},
+                "incidence": 0.01
+            },
+        ),
+        t2dm_gp_first=patients.with_these_clinical_events(
+            diabetes_t2_codes,
+            on_or_before=f"{index_date_variable} + 30 days",
+            return_first_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={
+                "date": {"earliest": "2015-01-01"},
+                "incidence": 0.01
+            },
+        ),
+        t2dm_gp_last=patients.with_these_clinical_events(
+            diabetes_t2_codes,
+            on_or_before=f"{index_date_variable} + 30 days",
+            return_last_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={
+                "date": {"earliest": "2015-01-01"},
+                "incidence": 0.01
+            },
+        ),
+        unknown_diabetes_gp_first=patients.with_these_clinical_events(
+            diabetes_unknown_codes,
+            on_or_before=f"{index_date_variable} + 30 days",
+            return_first_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={
+                "date": {"earliest": "2015-01-01"},
+                "incidence": 0.01
+            },
+        ),
+        unknown_diabetes_gp_last=patients.with_these_clinical_events(
+            diabetes_unknown_codes,
+            on_or_before=f"{index_date_variable} + 30 days",
+            return_last_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={
+                "date": {"earliest": "2015-01-01"},
+                "incidence": 0.01
+            },
+        ),
+        
         #### covid_diagnosis=
+        
         #### pneum_diagnosis=
+
         deregistered=patients.date_deregistered_from_all_supported_practices(
             date_format="YYYY-MM-DD"
         ),

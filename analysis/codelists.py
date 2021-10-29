335a3c6bf6295497b28e6bc03b273731cc3c9f48
Alison Wright <alison.wright@manchester.ac.uk>
2021-10-29 15:07:19 +0100

    codelists & variables updated

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -41,3 +41,175 @@ ethnicity_codes = codelist_from_csv(
 	column="Code",
 	category_column="Grouping_6",
 )
+    # /*1 "White"  2 "Mixed" 3 "Asian or Asian British"	4 "Black" 5 "Other"	.u "Unknown"*/
+
+# Smoking
+smoking_clear_codes = codelist_from_csv(
+    "codelists/opensafely-smoking-clear.csv", 
+    system="ctv3", 
+    column="CTV3Code", 
+    category_column="Category"
+)
+
+smoking_unclear_codes = codelist_from_csv(
+    "codelists/opensafely-smoking-unclear.csv", 
+    system="ctv3", 
+    column="CTV3Code", 
+    category_column="Category"
+)
+        # /* Current smoker - most recent smoking code is S
+        #    Ex-smoker - (most recent code is E) OR (most recent code is N AND an S or E code at any point)
+        #    Never smoker - most recent code is N AND doesn't have any S or E codes at any point
+        #    Missing - no S, E or N smoking codes on record */
+
+
+# Alcohol intake
+    ## Codes indicate hazardous alcohol drinking behaviour. 
+    ## Future studies that require to specify ex-drinker and current-drinker need to 
+    ## revisit and review this codelist.
+
+hazardous_alcohol_codes = codelist_from_csv(
+    "codelists/opensafely-hazardous-alcohol-drinking.csv", 
+    system="ctv3", 
+    column="code"
+)
+
+
+# HbA1c
+hba1c_new_codes = codelist(["XaPbt", "Xaeze", "Xaezd"], system="ctv3")
+hba1c_old_codes = codelist(["X772q", "XaERo", "XaERp"], system="ctv3")
+
+
+# History of CVD
+hist_cvd_codes = codelist_from_csv(
+    "codelists/opensafely-chronic-cardiac-disease.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+hist_cvd_codes_OPCS4 = codelist_from_csv(
+    "codelists/opensafely-cardiac-surgery-opcs-4-codes.csv",
+    system="opcs4",
+    column="Code",
+)
+##cardiac codelist may not be published yet - use personal code list
+hist_cvd_codes_OPCS4_2 = codelist_from_csv(
+    "local_codelists/uom-revascularisation.csv",
+    system="opcs4",
+    column="Code",
+)
+
+# History of renal disease (creatinine codes, RRT codes, CKD stage codes?-can't find CTV3 codes, SNOMED available)
+creatinine_codes = codelist(["XE2q5"], system="ctv3")
+
+ckd3_5_codes_hospital = codelist(["N183", "N184", "N185"], system="icd10")
+
+rrt_codes = codelist_from_csv(
+    "codelists/opensafely-renal-replacement-therapy.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+
+## OUTCOME CODES
+# CVD outcome
+# - Stroke
+    ## Need to check Matt's stroke codelists
+stroke_codes = codelist_from_csv(
+    "codelists/opensafely-incident-non-traumatic-stroke.csv",
+    system="ctv3",
+    column="CTV3ID",
+    category_column="type",
+)
+
+stroke_codes_hospital = codelist_from_csv(
+    "codelists/opensafely-stroke-secondary-care.csv", 
+    system="icd10", 
+    column="icd",
+    category_column="type",
+)
+
+# - MI
+mi_codes = codelist_from_csv(
+    "codelists/opensafely-myocardial-infarction-2.csv",
+    system="ctv3",
+    column="CTV3Code",
+)
+
+mi_codes_hospital = codelist(["I21", "I210", "I211", "I212", "I213", "I214", "I219", "I22", "I220", "I221", "I228", "I229", "I23", "I230", "I231", "I232", "I233", "I234", "I235", "I236", "I238"], system="icd10")
+   
+# - DVT
+dvt_codes = codelist(["G801.", "G80y.", "Xa6Yr", "X205e", "XE0VY", "G801z", "G801B", "XaJxo", "Xa9Bs", "XaQIV", "X205n", "X205m", "L417.", "G80y8", "Xa07G", "X76Lh", "Xacve", "XaZ43", "J4202", "XaDyD", "G8018", "G80y4", "G8019", "G80y6", "G8016", "G80y7", "G80y5", "G8017", "G801A", "Xacvd"], system="ctv3")
+
+dvt_codes_hospital = codelist(["I80","I800", "I801", "I802", "I803", "I808", "I809", "I821"], system="icd10")
+
+# - PE
+pe_codes = codelist(["X202y", "7A093", "X012b", "XE0Um", "X202x", "XaOYV", "X202z", "X012d"], system="ctv3")
+
+pe_codes_hospital = codelist(["I26", "I260", "I269"], system="icd10")
+
+# - HF
+hf_codes = codelist(["G1yz1", "G2101", "G2111", "G21z1", "G232.", "G234.", "G400.", "G41..", "G41y.", "G41yz", "G41z.", "G58..", "G580.", "G5800", "G5801", "G5802", "G5803", "G581.", "G5810", "G582.", "G58z.", "G5y4z", "H541z", "H584.", "H584z", "Q48y1", "SP111", "X102X", "X102Y", "X202k", "X202l", "X203E", "Xa0TT", "Xa0TU", "Xaacj", "Xaapw", "XaBLt", "XabM9", "XaEgY", "XafeB", "XaIIq", "XaIpn", "XaItG", "XaJ98", "XaJ99", "XaJ9G", "XaJ9H", "XaJ9I", "XaJ9J", "XaKNW", "XaO5n", "XaWyi", "XaXLu", "XaYYr", "XaYYs", "XaZih", "XE0V8", "XE0V9", "XE0WM", "XE0Wo", "XE1p9", "XE2QG", "XM1Qn"], system="ctv3")
+
+hf_codes_hospital = codelist(["I50", "I500", "I501", "I509"], system="icd10", column="icd")
+  
+
+# Pneumonia outcome
+pneumonia_codes = codelist_from_csv(
+    "codelists/opensafely-pneumonia-secondary-care.csv",
+    system="icd10",
+    column="ICD code",
+)
+
+# Renal outcome - AKI
+aki_codes = codelist(["J624.", "K034.", "K04..", "K040.", "K041.", "K04y.", "K04z.", "SP143", "X30Ip", "X30Ir", "X30Is", "X30Ix", "Xa85t", "Xaa8O", "Xaa8P", "Xaa8Q", "XaB3Y", "XaPwv", "XaZ6J", "XaZe5", "XaZPp", "XaZPs", "XaZPt", "XaZPu", "XaZSx", "XaZZ0", "XM08q", "XM199"], system="ctv3")
+
+aki_codes_hospital = codelist(["N17", "N170", "N171", "N172", "N178", "N179"], system="icd10")
+
+# Hepatic outcome
+    ## codelists need to be checked
+# - liver failure
+liver_transplant_codes = codelist_from_csv(
+    "local_codelists/uom-liver-transplant.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+# - chronic liver disease
+chronic_liver_dis_codes = codelist_from_csv(
+    "local_codelists/uom-liver-disease.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+# Mental illness
+# - anxiety
+    ## need to generate codelist
+# - depression
+"codelists/opensafely-depression.csv"
+# - affective/non-affective psychosis
+"codelists/opensafely-psychosis-schizophrenia-bipolar-affective-disease.csv"
+# - psychotropic medications
+    ## SNOMED codes required ##
+        #antidepressants, anxiolytics, antipsychotics, mood stabilisers, benzodiazepines
+    
+
+        ##"codelists/opensafely-selective-serotonin-reuptake-inhibitors-dmd.csv"
+        ##"codelists/opensafely-first-generation-antipsychotics-excluding-long-acting-depots-dmd.csv"
+        ##"codelists/user-orlamac-first-generation-antipsychotics-excluding-long-acting-depots.csv"
+    
+
+    ## have created a SNOMED codelist - needs to be checked
+
+
+# Symptoms of post-covid syndrome outcome
+# - sleep problems
+    # Insomnia, hypersomnia/hypersomnolence, sleep apnoea, sleep medications including CPAP
+
+    ## need to generate codelist
+    
+# - fatigue-like syndromes
+    ## need to generate codelist
+
+
+##### BMI SPECIFIED WITHIN COMMON_VARIABLES.PY #####
--- analysis/study_variables.py
+++ analysis/study_variables.py
@@ -17,7 +17,7 @@ def generate_study_variables(index_date_variable):
                 "category": {"ratios": {"M": 0.49, "F": 0.51}},
             },
         ),
-        ethnicity=patients.with_these_clinical_events(
+        ethnicity_clinical=patients.with_these_clinical_events(
 	        ethnicity_codes,	
 	        returning="category",
 	        find_last_match_in_period=True,
@@ -26,6 +26,10 @@ def generate_study_variables(index_date_variable):
 		        "category": {"ratios": {"1": 0.8, "5": 0.1, "3": 0.1}},
 		        "incidence": 0.8,
 	        },
+        ),
+        ethnicity_sus=patients.with_ethnicity_from_sus(
+            returning="group_6",
+            use_most_frequent_code=True,
         ),
         practice_id=patients.registered_practice_as_of(
             f"{index_date_variable}",
@@ -233,6 +237,313 @@ def generate_study_variables(index_date_variable):
                 "date": {"earliest": "2020-02-01"},
                 "incidence": 0.1,
             },
+        ),
+        ### OUTCOMES
+        ## CVD outcome (stroke, MI, DVT, PE, HF)
+        # stroke
+        stroke_gp=patients.with_these_clinical_events(
+            stroke_codes,
+            return_first_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            on_or_after=f"{index_date_variable}",
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        stroke_hospital=patients.admitted_to_hospital(
+            returning="date_admitted",
+            with_these_diagnoses=stroke_codes_hospital,
+            on_or_after=f"{index_date_variable}",
+            date_format="YYYY-MM-DD",
+            find_first_match_in_period=True,
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        stroke_ons=patients.with_these_codes_on_death_certificate(
+            stroke_codes_hospital,
+            returning="date_of_death",
+            date_format="YYYY-MM-DD",
+            match_only_underlying_cause=False,
+            on_or_after=f"{index_date_variable}",
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        stroke_outcome=patients.satisfying(
+            "stroke_gp OR stroke_hospital OR stroke_ons"
+        ),
+        stroke_outcome_date=patients.minimum_of(
+            "stroke_gp", "stroke_hospital", "stroke_ons"
+        ),  
+        # MI
+        mi_gp=patients.with_these_clinical_events(
+            mi_codes,
+            return_first_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            on_or_after=f"{index_date_variable}",
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        mi_hospital=patients.admitted_to_hospital(
+            returning="date_admitted",
+            with_these_diagnoses=mi_codes_hospital,
+            on_or_after=f"{index_date_variable}",
+            date_format="YYYY-MM-DD",
+            find_first_match_in_period=True,
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        mi_ons=patients.with_these_codes_on_death_certificate(
+            mi_codes_hospital,
+            returning="date_of_death",
+            date_format="YYYY-MM-DD",
+            match_only_underlying_cause=False,
+            on_or_after=f"{index_date_variable}",
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        mi_outcome=patients.satisfying(
+            "mi_gp OR mi_hospital OR mi_ons"
+        ),
+        mi_outcome_date=patients.minimum_of(
+            "mi_gp", "mi_hospital", "mi_ons"
+        ),       
+        # DVT
+        dvt_gp=patients.with_these_clinical_events(
+            dvt_codes,
+            return_first_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            on_or_after=f"{index_date_variable}",
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        dvt_hospital=patients.admitted_to_hospital(
+            returning="date_admitted",
+            with_these_diagnoses=dvt_codes_hospital,
+            on_or_after=f"{index_date_variable}",
+            date_format="YYYY-MM-DD",
+            find_first_match_in_period=True,
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        dvt_ons=patients.with_these_codes_on_death_certificate(
+            dvt_codes_hospital,
+            returning="date_of_death",
+            date_format="YYYY-MM-DD",
+            match_only_underlying_cause=False,
+            on_or_after=f"{index_date_variable}",
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        dvt_outcome=patients.satisfying(
+            "dvt_gp OR dvt_hospital OR dvt_ons"
+        ),
+        dvt_outcome_date=patients.minimum_of(
+            "dvt_gp", "dvt_hospital", "dvt_ons"
+        ),
+        # PE
+        pe_gp=patients.with_these_clinical_events(
+            pe_codes,
+            return_first_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            on_or_after=f"{index_date_variable}",
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        pe_hospital=patients.admitted_to_hospital(
+            returning="date_admitted",
+            with_these_diagnoses=pe_codes_hospital,
+            on_or_after=f"{index_date_variable}",
+            date_format="YYYY-MM-DD",
+            find_first_match_in_period=True,
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        pe_ons=patients.with_these_codes_on_death_certificate(
+            pe_codes_hospital,
+            returning="date_of_death",
+            date_format="YYYY-MM-DD",
+            match_only_underlying_cause=False,
+            on_or_after=f"{index_date_variable}",
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        pe_outcome=patients.satisfying(
+            "pe_gp OR pe_hospital OR pe_ons"
+        ),
+        pe_outcome_date=patients.minimum_of(
+            "pe_gp", "pe_hospital", "pe_ons"
+        ),
+        # HF
+        hf_gp=patients.with_these_clinical_events(
+            hf_codes,
+            return_first_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            on_or_after=f"{index_date_variable}",
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        hf_hospital=patients.admitted_to_hospital(
+            returning="date_admitted",
+            with_these_diagnoses=hf_codes_hospital,
+            on_or_after=f"{index_date_variable}",
+            date_format="YYYY-MM-DD",
+            find_first_match_in_period=True,
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        hf_ons=patients.with_these_codes_on_death_certificate(
+            hf_codes_hospital,
+            returning="date_of_death",
+            date_format="YYYY-MM-DD",
+            match_only_underlying_cause=False,
+            on_or_after=f"{index_date_variable}",
+            return_expectations={"date": {"earliest": "date_patient_index"}},
+        ),
+        hf_outcome=patients.satisfying(
+            "hf_gp OR hf_hospital OR hf_ons"
+        ),
+        hf_outcome_date=patients.minimum_of(
+            "hf_gp", "hf_hospital", "hf_ons"
+        ),
+        # Composite measure
+        composite_cvd_outcome=patients.satisfying(
+            "stroke_outcome OR mi_outcome OR dvt_outcome OR pe_outcome OR hf_outcome"
+        ),
+        composite_cvd_outcome_date=patients.minimum_of(
+            "stroke_outcome_date", "mi_outcome_date", "dvt_outcome_date", "pe_outcome_date", "hf_outcome_date"
+        ),  
+        ## Pneumonia outcome
+
+        ## Renal outcome
+
+        ## Hepatic outcome
+
+        ## Mental Illness
+
+        ## Symptoms of post-covid syndrome outcome
+
+        ## COVARIATES
+        # Smoking
+        smoking_status=patients.categorised_as(
+            {
+                "S": "most_recent_smoking_code = 'S'",
+                "E": """
+                    most_recent_smoking_code = 'E' OR (
+                    most_recent_smoking_code = 'N' AND ever_smoked
+                    )
+                """,
+                "N": "most_recent_smoking_code = 'N' AND NOT ever_smoked",
+                "M": "DEFAULT",
+            },
+            return_expectations={
+                "category": {"ratios": {"S": 0.6, "E": 0.1, "N": 0.2, "M": 0.1}}
+            },
+            most_recent_smoking_code=patients.with_these_clinical_events(
+                clear_smoking_codes,
+                find_last_match_in_period=True,
+                on_or_before=f"{index_date_variable} - 1 days",
+                returning="category",
+            ),
+            ever_smoked=patients.with_these_clinical_events(
+                filter_codes_by_category(clear_smoking_codes, include=["S", "E"]),
+                on_or_before=f"{index_date_variable} - 1 days",
+            ),
+        ),
+        smoking_status_date=patients.with_these_clinical_events(
+            clear_smoking_codes,
+            on_or_before=f"{index_date_variable} - 1 days",
+            return_last_date_in_period=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={
+            "incidence": 0.95,
+            },
+        ),
+        # Hazardous alcohol behaviour in the previous year
+        haz_alcohol_lastyear=patients.with_these_clinical_events(
+            hazardous_alcohol_codes,
+            between=[
+                f"{index_date_variable} - 1 year",
+                f"{index_date_variable} - 1 days",
+            ],
+        ),        
+        # HbA1c
+        hba1c_mmol_per_mol=patients.with_these_clinical_events(
+            hba1c_new_codes,
+            find_last_match_in_period=True,
+            on_or_before=f"{index_date_variable} - 1 days",
+            returning="numeric_value",
+            include_date_of_match=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={
+                "float": {"distribution": "normal", "mean": 40.0, "stddev": 20},
+                "incidence": 0.95,
+            },
+        ),
+        hba1c_percentage=patients.with_these_clinical_events(
+            hba1c_old_codes,
+            find_last_match_in_period=True,
+            on_or_before=f"{index_date_variable} - 1 days",
+            returning="numeric_value",
+            include_date_of_match=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={
+                "float": {"distribution": "normal", "mean": 5, "stddev": 2},
+                "incidence": 0.95,
+            },
+        ),
+        # BMI - continuous variable
+        bmi=patients.most_recent_bmi(
+            on_or_before=f"{index_date_variable} - 1 days",
+            minimum_age_at_measurement=16,
+            include_measurement_date=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={
+                "date": {},
+                "float": {"distribution": "normal", "mean": 35, "stddev": 10},
+                "incidence": 0.95,
+            },
+        ),  
+        # Alternative BMI - categorised 
+        bmi=patients.categorised_as(
+            {
+                "Not obese": "DEFAULT",
+                "Obese I (30-34.9)": """ bmi_value >= 30 AND bmi_value < 35""",
+                "Obese II (35-39.9)": """ bmi_value >= 35 AND bmi_value < 40""",
+                "Obese III (40+)": """ bmi_value >= 40 AND bmi_value < 100""",
+                    set maximum to avoid any impossibly extreme values being classified as obese
+            },
+            bmi_value=patients.most_recent_bmi(
+                on_or_before=f"{index_date_variable} - 1 days", 
+                minimum_age_at_measurement=16
+            ),
+            return_expectations={
+                "rate": "universal",
+                "category": {
+                    "ratios": {
+                        "Not obese": 0.7,
+                        "Obese I (30-34.9)": 0.1,
+                        "Obese II (35-39.9)": 0.1,
+                        "Obese III (40+)": 0.1,
+                    }
+                },
+            },
+        ),
+        # History of CVD
+        hist_cvd=patients.satisfying(
+            "cvd OR cvd_opcs",
+            cvd=patients.with_these_clinical_events(
+                hist_cvd_codes, on_or_before=f"{index_date_variable} - 1 days",
+            ),
+            cvd_opcs=patients.admitted_to_hospital(
+                with_these_procedures=hist_cvd_codes_OPCS4_2, on_or_before=f"{index_date_variable} - 1 days",
+            ),           
+        ),        
+        # History of renal disease (creatinine measurement, RRT-dialysis/transplant, CKD stage codes?-no CTV3 codes, SNOMED available)
+        creatinine=patients.with_these_clinical_events(
+            creatinine_codes,
+            find_last_match_in_period=True,
+            on_or_before=f"{index_date_variable} - 1 days",
+            returning="numeric_value",
+            include_date_of_match=True,
+            date_format="YYYY-MM-DD",
+            return_expectations={
+                "float": {"distribution": "normal", "mean": 60.0, "stddev": 15},
+                "incidence": 0.95,
+            },
+        ),
+        hist_renal=patients.satisfying(
+            "ckd_hospital OR hist_rrt",
+            ckd_hospital=patients.admitted_to_hospital(
+                with_these_diagnoses=ckd3_5_codes_hospital, on_or_before=f"{index_date_variable} - 1 days",
+            ),
+            hist_rrt=patients.with_these_clinical_events(
+                rrt_codes, on_or_before=f"{index_date_variable} - 1 days",
+            ),
         ),
     )
     return study_variables
